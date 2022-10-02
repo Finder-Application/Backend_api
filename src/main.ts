40e6e6da1744eda887ae8510a4be3d1c9ea39977
chinh.nguyen@team.enouvo.com <chinhnguyen@ChinhNguyen-FE.local>
2022-10-02 11:19:52 +0700

    Add cicd action connect to ssh

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ export async function bootstrap(): Promise<NestExpressApplication> {
     new ExpressAdapter(),
     { cors: true },
   );
+  app.enableCors();
   app.enable('trust proxy'); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
   app.use(helmet());
   // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
